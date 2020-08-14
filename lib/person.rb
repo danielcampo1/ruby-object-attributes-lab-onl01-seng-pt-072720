ad2774539e61a5069c33aa39dfb5b93c30259740
daniel campo <campo_daniel94@yahoo.com>
2020-08-14 22:04:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,14 @@
 class Person
-  def name=(human)
-    @name = human
+  def name=(person)
+    @name = person
+  end
+  
+  def name
+    @name
+  end
 end
+
+beyonce = Person.new
+beyonce.person = beyonce
+
+puts beyonce.person
